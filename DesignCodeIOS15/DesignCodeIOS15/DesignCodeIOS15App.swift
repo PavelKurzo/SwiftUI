7e6d51d557a6b35e5ca8156bc2efce00853973b6
Pavel <pavel.kurzo@mail.ru>
2022-09-01 14:34:28 +0300

    Configuring Git

---
+++ DesignCodeIOS15/DesignCodeIOS15/DesignCodeIOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeIOS15App.swift
+//  DesignCodeIOS15
+//
+//  Created by Павел Курзо on 1.09.22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeIOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
