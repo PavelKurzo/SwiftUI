ca251a2c02c2b5afce7650572347e9c7a052f4cb
Pavel <pavel.kurzo@mail.ru>
2022-09-01 15:48:20 +0300

    Usage of Shape, Stroke and Blend Mode

--- DesignCodeIOS15/DesignCodeIOS15/ContentView.swift
+++ DesignCodeIOS15/DesignCodeIOS15/ContentView.swift
@@ -16,6 +16,8 @@ struct ContentView: View {
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26.0, height: 26.0)
                 .cornerRadius(20.0)
+                .padding(9)
+                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 20, style: .continuous))
             Text("SwiftUI for IOS 15")
                 .font(.largeTitle)
                 .fontWeight(.bold)
@@ -31,7 +33,6 @@ struct ContentView: View {
                 .lineLimit(2)
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .foregroundColor(.secondary)
-                .offset(y: 50)
         }
         .padding(.all, 20.0)
         .padding(.vertical, 20)
@@ -40,6 +41,11 @@ struct ContentView: View {
 //        .cornerRadius(30.0)
 //        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
         .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .stroke(.linearGradient(colors: [.white.opacity(0.3), .black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
+                .blendMode(.overlay)
+        )
         .padding(.horizontal, 20)
         .background(
             Image("Blob 1")
