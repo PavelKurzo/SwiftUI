98c802da0c70ac8fdbd784e964e17d599b40b099
Pavel <pavel.kurzo@mail.ru>
2022-09-06 17:30:19 +0300

    Animation for navbar

---
+++ DesignCodeIOS15/DesignCodeIOS15/Model/PreferenceKeys.swift
@@ -0,0 +1,15 @@
+//
+//  PreferenceKeys.swift
+//  DesignCodeIOS15
+//
+//  Created by Павел Курзо on 6.09.22.
+//
+
+import SwiftUI
+
+struct ScrollPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
