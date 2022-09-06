d44e8cae0de83e92066ef048061638b7c4b5c188
Pavel <pavel.kurzo@mail.ru>
2022-09-06 23:29:26 +0300

    Parallax and Rotation 3D Effect

--- DesignCodeIOS15/DesignCodeIOS15/Model/Course.swift
+++ DesignCodeIOS15/DesignCodeIOS15/Model/Course.swift
@@ -19,6 +19,6 @@ struct Course: Identifiable {
 var courses = [
 Course(title: "SwiftUI for IOS 15", subtitle: "20 sections - 3 hours", text: "Build an IOS app for IOS 15 with custom layouts, animations and ...", image: "Illustration 5", logo: "Logo 2"),
 Course(title: "UI Design for IOS 15", subtitle: "20 sections - 3 hours", text: "Design an IOS app fir IOS 15 with custom layouts, animations and ...", image: "Illustration 3", logo: "Logo 4"),
-Course(title: "Flutter for designers", subtitle: "20 sections - 3 hours", text: "Flutter is a relatively new toolkit that makes it easy to build cross-platform apps that look gorgeous and is easy to use", image: "Illustration 5", logo: "Logo 1"),
+Course(title: "Flutter for designers", subtitle: "20 sections - 3 hours", text: "Flutter is a relatively new toolkit that makes it easy to build cross-platform apps that look gorgeous and is easy to use", image: "Illustration 1", logo: "Logo 1"),
 Course(title: "React Hooks Advanced", subtitle: "20 sections - 3 hours", text: "Build an IOS app for IOS 15 with custom layouts, animations and ...", image: "Illustration 2", logo: "Logo 3"),
 ]
