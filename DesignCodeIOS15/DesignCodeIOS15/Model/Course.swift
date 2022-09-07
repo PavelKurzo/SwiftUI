0bb568f10e017a9c12414fbe45fc5056b428151a
Pavel <pavel.kurzo@mail.ru>
2022-09-07 15:52:52 +0300

    Matched Geometry with Data

--- DesignCodeIOS15/DesignCodeIOS15/Model/Course.swift
+++ DesignCodeIOS15/DesignCodeIOS15/Model/Course.swift
@@ -13,12 +13,20 @@ struct Course: Identifiable {
     var subtitle: String
     var text: String
     var image: String
+    var background: String
     var logo: String
 }
 
+var featuredCourses = [
+    Course(title: "SwiftUI for IOS 15", subtitle: "20 sections - 3 hours", text: "Build an IOS app for IOS 15 with custom layouts, animations and ...", image: "Illustration 5", background: "Background 5", logo: "Logo 2"),
+    Course(title: "UI Design for IOS 15", subtitle: "20 sections - 3 hours", text: "Design an IOS app fir IOS 15 with custom layouts, animations and ...", image: "Illustration 3", background: "Background 4", logo: "Logo 4"),
+    Course(title: "Flutter for designers", subtitle: "20 sections - 3 hours", text: "Flutter is a relatively new toolkit that makes it easy to build cross-platform apps that look gorgeous and is easy to use", image: "Illustration 1", background: "Background 1", logo: "Logo 1"),
+    Course(title: "React Hooks Advanced", subtitle: "20 sections - 3 hours", text: "Build an IOS app for IOS 15 with custom layouts, animations and ...", image: "Illustration 2", background: "Background 2", logo: "Logo 3"),
+]
+
 var courses = [
-Course(title: "SwiftUI for IOS 15", subtitle: "20 sections - 3 hours", text: "Build an IOS app for IOS 15 with custom layouts, animations and ...", image: "Illustration 5", logo: "Logo 2"),
-Course(title: "UI Design for IOS 15", subtitle: "20 sections - 3 hours", text: "Design an IOS app fir IOS 15 with custom layouts, animations and ...", image: "Illustration 3", logo: "Logo 4"),
-Course(title: "Flutter for designers", subtitle: "20 sections - 3 hours", text: "Flutter is a relatively new toolkit that makes it easy to build cross-platform apps that look gorgeous and is easy to use", image: "Illustration 1", logo: "Logo 1"),
-Course(title: "React Hooks Advanced", subtitle: "20 sections - 3 hours", text: "Build an IOS app for IOS 15 with custom layouts, animations and ...", image: "Illustration 2", logo: "Logo 3"),
+    Course(title: "SwiftUI for IOS 15", subtitle: "20 sections - 3 hours", text: "Build an IOS app for IOS 15 with custom layouts, animations and ...", image: "Illustration 9", background: "Background 5", logo: "Logo 2"),
+    Course(title: "UI Design for IOS 15", subtitle: "20 sections - 3 hours", text: "Design an IOS app fir IOS 15 with custom layouts, animations and ...", image: "Illustration 2", background: "Background 3", logo: "Logo 2"),
+    Course(title: "Flutter for designers", subtitle: "20 sections - 3 hours", text: "Flutter is a relatively new toolkit that makes it easy to build cross-platform apps that look gorgeous and is easy to use", image: "Illustration 3", background: "Background 4", logo: "Logo 4"),
+    Course(title: "React Hooks Advanced", subtitle: "20 sections - 3 hours", text: "Build an IOS app for IOS 15 with custom layouts, animations and ...", image: "Illustration 1", background: "Background 1", logo: "Logo 1"),
 ]
